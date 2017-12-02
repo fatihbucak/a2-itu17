f8cca70c33261ce170b0af5269722fc4bb5e3928
fatihbucak <32177116+fatihbucak@users.noreply.github.com>
2017-12-02 08:46:47 +0300

    Added some codes necessary for assignment 2

--- a2_process_data.py
+++ a2_process_data.py
@@ -19,11 +19,134 @@ with open("a2_input.csv") as input_file:
 ### Print your results using the print function.
 #######################################################
 
-print("This assignment (assignment 2) hasn't been finished.")
-print("All it can do is print out the contents of a couple of cells of the file a2_input.csv:")
-print("Cell at index 0,0:")
+html_base='''<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>%s</title>
+        %s
+    </head>
+    <body>
+        %s
+        %s
+    </body>
+</html>
+'''
+
+        
+def calculate_the_sum(list):
+    total=0.0
+    for element in list:
+        total += float(element)
+    return total
+
+def get_column_as_a_list(index_of_column):
+    column_list = []
+    for row in range(len(contents)):
+        column_list += [contents[row][index_of_column]]
+    return column_list
+
+def save_html_file(code,file_name):
+    html_file = open(file_name,"w")
+    print(code,file=html_file)
+    html_file.close()
+
+def get_row(index):
+    row = ""
+    for column in range(len(contents[index])):
+        row += "				<td>" + str(contents[index][column]) + "</td>\n"
+    return row
+
+def get_table_code():
+    base_table = '''
+    	<table>
+        %s
+    	</table>
+    '''
+    rows_of_table = '''
+			<tr>
+				<th>Movies About Time Travel</th>
+				<th>Year</th>
+				<th>IMDb</th>
+				<th>Duration</th>
+			</tr>
+    	'''
+    for index in range(len(contents)):
+        rows_of_table += "	<tr>\n" + get_row(index) + "\n			</tr>\n		"
+    return base_table % (rows_of_table)
+
+average_of_imdb = calculate_the_sum(get_column_as_a_list(2))/len(contents)
+
+def number_of_movies_in_a_year(year):
+    index = 0
+    total=0
+    while index < len(contents):
+        if str(contents[index][1])==str(year):
+            total += 1
+        index += 1
+    return total
+
+def number_of_movies_longer(min):
+    index = 0
+    total=0
+    while index < len(contents):
+        if int(contents[index][3]) > min:
+            total += 1
+        index += 1
+    return total
+
+def get_some_content():
+    list='''
+        <ul>
+            <li>%s</li>
+            <li>%s</li>
+            <li>%s</li>
+        </ul>
+    '''
+    return list % ("Number of Movies About Time Travel in 2016: " + str(number_of_movies_in_a_year(2016)), "Average of IMDb: " + str(average_of_imdb), "Number of Movies that is longer than 120 mins: " + str(number_of_movies_longer(120)))
+    
+
+style_code = '''<style>
+            td{
+                border:2px solid black;
+                padding:3px;
+            }
+            th{
+                border:2px solid black;
+                padding:3px;
+            }
+        </style>
+'''
+print("The below is variable contents")
+print(contents)
+print("The below is the first row of variable contents")
+print(contents[0])
+print("The below is entry which is in both first row and first column")
 print(contents[0][0])
-print("Cell at index 0,1:")
-print(contents[0][1])
-print("Cell at index 1,0:")
-print(contents[1][0])
+print("The below is the type of the variable contents")
+print(type(contents))
+print("The below is the type of the first row in the contents")
+print(type(contents[0]))
+print("The below is the type of the entry which is in both first row and first column")
+print(type(contents[0][0]))
+print("The below is the entry which is in second row and third column")
+print(contents[2][3])
+print("The below is the type of the entry which is in third row and second column")
+print(type(contents[3][2]))
+print("The below is sum of durations")
+sum_of_durations = calculate_the_sum(get_column_as_a_list(3))
+print(sum_of_durations)
+print("The below is average of imdb")
+print(average_of_imdb)
+# print(contents[10][10]) There is an error because the value doesn't exist. Error message: list index out of range
+thirty_third_line_of_content = "Movie: " + contents[33][0] + "\nYear: " + contents[33][1] + "\nImdb: " + contents[33][2] + "\nDuration: " + contents[33][3]
+print(thirty_third_line_of_content)
+# print(contents[1][0] + 3) There is an error because string can not be added an integer. Error message: must be str, not int
+product_of_string_and_integer = contents[143][0]*3
+print("The below is product of string and integer")
+print(product_of_string_and_integer)
+print(type(sum_of_durations))
+print(type(average_of_imdb))
+print(type(product_of_string_and_integer))
+help(average_of_imdb)
+save_html_file(html_base % ("Movies About Time Travel", style_code, get_some_content(), get_table_code()),"a2_output.html")
